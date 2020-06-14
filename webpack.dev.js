4e5e4aeebf937a5bb5ad5717cbfcfe6127e2a48c
Melanie <melanie_foeldes@gmx.de>
2020-06-14 11:33:40 +0200

    add dev and prod files

---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/client/index.js",
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
+const htmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: "production",
     entry: "./src/client/index.js",
     module: {
         rules: [
@@ -11,5 +14,11 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
